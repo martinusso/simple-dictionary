f5e571f83ac36f744fbffce71ede364fec621303
Breno Martinusso <martinusso@gmail.com>
2015-01-19 09:35:24 -0200

    remove interface

--- src/SimpleDictionary.pas
+++ src/SimpleDictionary.pas
@@ -16,13 +16,7 @@ uses
   Classes, Variants;
 
 type
-  ISimpleDictionary = interface
-    ['{63599D0E-F18C-42A3-930B-5788155C4671}']
-    function Contains(const Key: Variant): Boolean;
-    function IndexOf(const Key: Variant): Integer;
-  end;
-
-  TSimpleDictionaryBase = class(TInterfacedObject, ISimpleDictionary)
+  TSimpleDictionaryBase = class(TObject)
   private
     procedure Error(const Msg: string; Data: Integer);
   protected
